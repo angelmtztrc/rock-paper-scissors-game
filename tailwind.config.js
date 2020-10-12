6b6d56016b44dbbdf9e6604a58ec2fd1e194a193
Angel Martinez <angel.codes139@gmail.com>
2020-10-12 13:29:38 -0500

    🔧 config(tailwindcss): adding custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
   },
   purge: ['src/*.svelte'],
   theme: {
-    extend: {}
+    extend: {
+      gradientColorStops: theme => ({
+        'scissors-primary': '#ec9e0e',
+        'scissors-secondary': '#eca922',
+        'paper-primary': '#4865f4',
+        'paper-secondary': '#5671f5',
+        'rock-primary': '#dc2e4e',
+        'rock-secondary': '#dd405d',
+        'lizard-primary': '#834fe3',
+        'lizard-secondary': '#8c5de5',
+        'cyan-primary': '#40b9ce',
+        'cyan-secondary': '#52bed1',
+        'radial-primary': '#1f3756',
+        'radial-secondary': '#141539',
+        'dark': '#3b4363',
+        'score': '#2a46c0',
+        'gray-outline': '#606e85'
+      })
+    }
   },
   variants: {},
   plugins: []
