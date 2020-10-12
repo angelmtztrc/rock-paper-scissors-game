512b45c022b1fb4f0db40d7dd675d94bc10208d9
Angel Martinez <angel.codes139@gmail.com>
2020-10-12 12:55:46 -0500

    ✨ feature(tailwindcss): adding tailwindcss to the project

--- src/App.svelte
+++ src/App.svelte
@@ -2,29 +2,6 @@
 	export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main class="min-h-screen flex justify-center items-center">
+	<h1 class="text-4xl text-orange-600 font-bold">Hello {name}!</h1>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
